f0cf2610bf4df3227dcb87cd227d1f7f8d43cce7
Toan <nguyentoan04.0003@email.com>
2025-03-02 13:22:19 +0700

    webcam call

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://loginx.onrender.com';
-// const URL = 'http://localhost:8123';
+// const URL = 'https://loginx.onrender.com';
+const URL = 'http://localhost:8123';
 
 export const socket = io(URL);
