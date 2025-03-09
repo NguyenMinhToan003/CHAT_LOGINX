27a434f1c84a5b71f9993702617efb23666346de
Toan <nguyentoan04.0003@email.com>
2025-03-09 11:57:14 +0700

    recommit

--- src/api.js
+++ src/api.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
-const axiosInstance = axios.create({
-	baseURL: `${import.meta.env.VITE_SERVER_HOST}/api`,
-	withCredentials: true
-})
+
+const host = `${import.meta.env.VITE_SERVER_HOST}/api`;
 
 //////////! room
 export const createRoomChat = async (type,name,avartar = null,admins,member) => {
-	const response = await axiosInstance.post(`/roomchat/create`, {
+	const response = await axios.post(`${host}/roomchat/create`, {
 		type: type,
 		name: name,
 		avartar: avartar,
@@ -17,18 +15,18 @@ export const createRoomChat = async (type,name,avartar = null,admins,member) => 
 }
 
 export const getRoomChat = async (id) => {
-	const response = await axiosInstance.get(`/roomchat/getRoom?roomId=${id}`)
+	const response = await axios.get(`${host}/roomchat/getRoom?roomId=${id}`)
 	return response?.data;
 }
 export const getAllMessage = async (roomId,userId) => {
-	const response = await axiosInstance.post(`/message/all`, {
+	const response = await axios.post(`${host}/message/all`, {
 		roomId: roomId,
 		userId: userId
 	})
 	return response?.data;
 }
 export const createMessage = async (roomId, userId, message) => {
-	const response = await axiosInstance.post(`/message/create`, {
+	const response = await axios.post(`${host}/message/create`, {
 		roomId: roomId,
 		sender: userId,
 		content: message
@@ -36,32 +34,34 @@ export const createMessage = async (roomId, userId, message) => {
 	return response?.data;
 }
 export const getRoomChatByUserId = async (userId) => {
-	const response = await axiosInstance.get(`/roomchat/getRoomChatByUserId?userId=${userId}`)
+	const response = await axios.get(`${host}//roomchat/getRoomChatByUserId?userId=${userId}`)
 	return response?.data;
 }
 //////////! user
 export const getAllUser = async () => {
-	const response = await axiosInstance.get(`/user/all`)
+	const response = await axios.get(`${host}/user/all`)
 	return response?.data;
 }
 export const getDataUser = async (id) => {
-	const response = await axiosInstance.get(`/user/?id=${id}`)
+	const response = await axios.post(`${host}/user`, {
+			id:id
+	})
 	return response?.data;
 }
 
 //////////! call Stringee
 export const createRoomStringee = async () => {
-	const response = await axiosInstance.post(`/roomVideoCall/create-room`)
+	const response = await axios.post(`${host}/roomVideoCall/create-room`)
 	return response?.data;
 }
 export const getUserTokenStringee = async (userId) => {
-	const response = await axiosInstance.post(`/roomVideoCall/get-user-token`, {
+	const response = await axios.post(`${host}/roomVideoCall/get-user-token`, {
 		userId: userId
 	})
 	return response?.data;
 }
 export const getRoomTokenStringee = async (roomId) => {
-	const response = await axiosInstance.post(`/roomVideoCall/get-room-token`, {
+	const response = await axios.post(`${host}/roomVideoCall/get-room-token`, {
 		roomId: roomId
 	})
 	return response?.data;
@@ -69,12 +69,6 @@ export const getRoomTokenStringee = async (roomId) => {
 
 //////! peerjs server
 export const getIceServer = async () => {
-	const response = await axiosInstance.get(`/peer-server/get-ice-server`)
-	return response?.data;
-}
-
-//////! auth
-export const getUserWithCookieServer = async () => {
-	const response = await axiosInstance.post(`/auth/user`)
+	const response = await axios.get(`${host}/peer-server/get-ice-server`)
 	return response?.data;
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import User from "../User/User";
 import Home from "../home/Home";
@@ -6,20 +6,22 @@ import RoomChat from "../roomchat/RoomChat";
 import RoomChatId from "../roomchat/RoomChatId";
 import RoomChatVideoCall from "../roomchat/RoomChatVideoCall";
 import Profile from "../VideoCall/VideoCall";
-import HandleLoginPassport from "../auth/HandleLoginPassport";
+
+
 
 const AppRouter = () => {
   
   return (
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/user" element={<User />} />
+        <Route path="/user/:id" element={<User />} />
         <Route path='/roomchats' element={<RoomChat />} />
         <Route path='/roomchats/:id' element={<RoomChatId />} />
         <Route path='/video-call/:id' element={<Profile/>} />
         <Route path='/roomChatVideoCall/:id' element={<RoomChatVideoCall />} />
-        <Route path='/login' element={<HandleLoginPassport />} />
       </Routes>
+
   );
 };
 
