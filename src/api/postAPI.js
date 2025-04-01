02c35fc6f6afcda100bea3751e23732533993269
Toan <nguyentoan04.0003@email.com>
2025-04-01 22:58:06 +0700

    fix-profile

--- src/api/postAPI.js
+++ src/api/postAPI.js
@@ -28,7 +28,39 @@ export const  deletePost=async ({postId,authorId})=>{
   })
 
   return response.data
+}
+
+   
 
   
 
-}
+    // API tạo bài viết mới
+    export const createPost = async (formData) => {
+        try {
+            const response = await axiosInstance.post('/post/create', formData, {
+                headers: { 'Content-Type': 'multipart/form-data' },
+            });
+            return response?.data;
+        } catch (error) {
+            console.error('Error creating post:', error);
+            throw error;
+        }
+    };
+
+    // API cập nhật ảnh đại diện người dùng
+    export const updateProfilePicture = async (userId, name,files) => {
+        try {
+          const formData = new FormData();
+          formData.append('userId', userId);
+          formData.append('name', name);
+          formData.append('files', files);
+      
+          const response = await axiosInstance.post('/user/edit', formData, {
+            headers: { 'Content-Type': 'multipart/form-data' },
+          });
+          return response?.data;
+        } catch (error) {
+          console.error('Error updating profile picture:', error);
+          throw error;
+        }
+      };
