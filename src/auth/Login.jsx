14279fc07793e08065dc51d8ee9fcf9ed115f267
Toan <nguyentoan04.0003@email.com>
2025-04-11 13:01:11 +0700

    add env callback zalo

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -95,7 +95,7 @@ const Login = () => {
   }
   const loginWithZalo = async () => {
     try {
-      window.location.href = 'https://oauth.zaloapp.com/v4/permission?app_id=3009287701854810432&redirect_uri=http://localhost:8123/api/auth/zalo/callback&state=true'
+      window.location.href = `https://oauth.zaloapp.com/v4/permission?app_id=3009287701854810432&redirect_uri=${host}/auth/zalo/callback&state=true`
     } catch (error) {
       console.error('Error during login:', error)
     }
